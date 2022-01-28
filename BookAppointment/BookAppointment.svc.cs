48553d4303b7d65a1ed6ea1aecc7ccb45ea84db6
mehul <mehul@DESKTOP-3JUSL5H>
2022-01-28 18:30:18 +0530

    Add project files.

---
+++ BookAppointment/BookAppointment.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace BookAppointment
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "BookAppointment" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select BookAppointment.svc or BookAppointment.svc.cs at the Solution Explorer and start debugging.
+    public class BookAppointment : IBookAppointment
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
